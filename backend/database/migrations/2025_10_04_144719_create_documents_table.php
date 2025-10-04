34d2e1d7e5f4a99d7273dbc8efb4b49e6629e78d
Danilo Patanè <d.patane98@gmail.com>
2025-10-04 16:55:44 +0200

    Base structure

---
+++ backend/database/migrations/2025_10_04_144719_create_documents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('note')->nullable();
+            $table->string('path')->nullable();
+            $table->string('type')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
